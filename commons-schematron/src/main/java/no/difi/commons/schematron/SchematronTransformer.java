a02ad177e53ccbacafbdf8d89398f7e12f005268
Erlend Klakegg Bergheim <erlend@klakegg.net>
2015-08-01 09:56:12 +0200

    Refactoring and adding some more helper methods.

--- commons-schematron/src/main/java/no/difi/commons/schematron/SchematronTransformer.java
+++ commons-schematron/src/main/java/no/difi/commons/schematron/SchematronTransformer.java
@@ -1,6 +1,8 @@
 package no.difi.commons.schematron;
 
 import net.sf.saxon.TransformerFactoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
@@ -8,13 +10,26 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class SchematronTransformer {
 
-    private static TransformerFactory transformerFactory = new TransformerFactoryImpl();
+    private static Logger logger = LoggerFactory.getLogger(SchematronTransformer.class);
+
+    private static TransformerFactory transformerFactory;
+
+    static {
+        try {
+            // Try to load Saxon implementation before defaulting to default implementation.
+            transformerFactory = (TransformerFactoryImpl) Class.forName("net.sf.saxon.TransformerFactoryImpl").getConstructor().newInstance();
+            logger.info("Using Saxon transformer.");
+        } catch (Exception e) {
+            transformerFactory = TransformerFactory.newInstance();
+            logger.info("Using default transformer.");
+        }
+    }
 
     private Transformer step1, step2, step3;
 
@@ -24,7 +39,21 @@ public class SchematronTransformer {
         step3 = transformerFactory.newTransformer(new StreamSource(getClass().getResource("/iso-schematron-xslt2/iso_svrl_for_xslt2.xsl").getFile()));
     }
 
-    public synchronized void transform(File inputFile, File outputFile) throws TransformerException {
+    public void transform(File inputFile, File outputFile) throws TransformerException, IOException {
+        transform(inputFile.toPath(), outputFile.toPath());
+    }
+
+    public void transform(Path inputFile, Path outputFile) throws TransformerException, IOException {
+        OutputStream outputStream = Files.newOutputStream(outputFile);
+        transform(inputFile, outputStream);
+        outputStream.close();
+    }
+
+    public void transform(Path inputFile, OutputStream outputStream) throws TransformerException {
+        transform(inputFile.toFile(), outputStream);
+    }
+
+    public synchronized void transform(File inputFile, OutputStream outputStream) throws TransformerException {
         ByteArrayInputStream byteArrayInputStream;
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
@@ -37,7 +66,6 @@ public class SchematronTransformer {
 
         byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
 
-        step3.transform(new StreamSource(byteArrayInputStream), new StreamResult(outputFile));
+        step3.transform(new StreamSource(byteArrayInputStream), new StreamResult(outputStream));
     }
-
 }
