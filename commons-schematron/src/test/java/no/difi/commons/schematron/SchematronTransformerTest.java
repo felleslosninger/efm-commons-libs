bcbd81412994c04575bddc2c956a183e64770d6e
Erlend Klakegg Bergheim <erlend@klakegg.net>
2015-08-02 22:32:36 +0200

    Adding test.

---
+++ commons-schematron/src/test/java/no/difi/commons/schematron/SchematronTransformerTest.java
@@ -0,0 +1,12 @@
+package no.difi.commons.schematron;
+
+import org.testng.annotations.Test;
+
+public class SchematronTransformerTest {
+
+    @Test
+    public void testConstructor() throws Exception {
+        new SchematronTransformer();
+    }
+
+}
